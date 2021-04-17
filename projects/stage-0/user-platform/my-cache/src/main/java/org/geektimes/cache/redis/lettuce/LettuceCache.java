7a1142114fd55d80a10103cbae9191e080974fd7
yinifeng <78381092@qq.com>
2021-04-17 14:13:17 +0800

    feat(4.14): 整合lettuceRedis客户端

--- projects/stage-0/user-platform/my-cache/src/main/java/org/geektimes/cache/redis/lettuce/LettuceCache.java
+++ projects/stage-0/user-platform/my-cache/src/main/java/org/geektimes/cache/redis/lettuce/LettuceCache.java
@@ -1,7 +1,11 @@
 package org.geektimes.cache.redis.lettuce;
 
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
 import org.geektimes.cache.AbstractCache;
 import org.geektimes.cache.ExpirableEntry;
+import org.geektimes.cache.serializer.JdkSerializationRedisSerializer;
+import org.geektimes.cache.serializer.RedisSerializer;
 
 import javax.cache.CacheException;
 import javax.cache.CacheManager;
@@ -10,30 +14,51 @@ import java.io.Serializable;
 import java.util.Set;
 
 public class LettuceCache<K extends Serializable, V extends Serializable> extends AbstractCache<K, V> {
+    
+    private final StatefulRedisConnection<byte[], byte[]> connection;
+    
+    private final RedisCommands<byte[], byte[]> redisCommands;
+
+    private RedisSerializer<Object> redisSerializer = new JdkSerializationRedisSerializer();
+
 
     public LettuceCache(CacheManager cacheManager, String cacheName, 
-                        Configuration<K, V> configuration) {
+                        Configuration<K, V> configuration,StatefulRedisConnection<byte[], byte[]> connection) {
         super(cacheManager, cacheName, configuration);
+        this.connection = connection;
+        this.redisCommands = connection.sync();
     }
 
     @Override
     protected boolean containsEntry(K key) throws CacheException, ClassCastException {
-        return false;
+        byte[] keyBytes = redisSerializer.serialize(key);
+        return this.redisCommands.exists(keyBytes) > 0;
     }
 
     @Override
     protected ExpirableEntry<K, V> getEntry(K key) throws CacheException, ClassCastException {
-        return null;
+        byte[] keyBytes = redisSerializer.serialize(key);
+        return getEntry(keyBytes);
+    }
+
+    protected ExpirableEntry<K, V> getEntry(byte[] keyBytes) throws CacheException, ClassCastException {
+        byte[] valueBytes = this.redisCommands.get(keyBytes);
+        return ExpirableEntry.of((K)redisSerializer.deserialize(keyBytes), (V)redisSerializer.deserialize(valueBytes));
     }
 
     @Override
     protected void putEntry(ExpirableEntry<K, V> entry) throws CacheException, ClassCastException {
-
+        byte[] keyBytes = redisSerializer.serialize(entry.getKey());
+        byte[] valueBytes = redisSerializer.serialize(entry.getValue());
+        this.redisCommands.set(keyBytes, valueBytes);
     }
 
     @Override
     protected ExpirableEntry<K, V> removeEntry(K key) throws CacheException, ClassCastException {
-        return null;
+        byte[] keyBytes = redisSerializer.serialize(key);
+        ExpirableEntry<K, V> oldEntry = getEntry(keyBytes);
+        this.redisCommands.del(keyBytes);
+        return oldEntry;
     }
 
     @Override
@@ -44,5 +69,10 @@ public class LettuceCache<K extends Serializable, V extends Serializable> extend
     @Override
     protected Set<K> keySet() {
         return null;
+    }
+
+    @Override
+    protected void doClose() {
+        this.connection.close();
     }
 }
--- projects/stage-0/user-platform/my-cache/src/main/java/org/geektimes/cache/redis/lettuce/LettuceCacheManager.java
+++ projects/stage-0/user-platform/my-cache/src/main/java/org/geektimes/cache/redis/lettuce/LettuceCacheManager.java
@@ -1,5 +1,10 @@
 package org.geektimes.cache.redis.lettuce;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
 import org.geektimes.cache.AbstractCacheManager;
 
 import javax.cache.Cache;
@@ -9,14 +14,28 @@ import java.net.URI;
 import java.util.Properties;
 
 public class LettuceCacheManager extends AbstractCacheManager {
+    static final RedisCodec<byte[], byte[]> CODEC = ByteArrayCodec.INSTANCE;
+    
+    private RedisClient redisClient;
     
     public LettuceCacheManager(CachingProvider cachingProvider, URI uri, ClassLoader classLoader, 
                                Properties properties) {
         super(cachingProvider, uri, classLoader, properties);
+        //this.uri =
     }
 
     @Override
     protected <K, V, C extends Configuration<K, V>> Cache doCreateCache(String cacheName, C configuration) {
-        return null;
+        this.redisClient = RedisClient.create(RedisURI.create(this.getURI()));
+        StatefulRedisConnection<byte[], byte[]> connection = redisClient.connect(CODEC);
+        return new LettuceCache(this,cacheName,configuration,connection);
+    }
+
+    @Override
+    protected void doClose() {
+        if (redisClient != null){
+            redisClient.shutdown();
+        }
     }
 }
+ 
